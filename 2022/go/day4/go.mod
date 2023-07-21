05dceea509788c35b1f16776a758dbc11a219a34
Steven Yeu <syeu@ucsd.edu>
2023-07-20 22:14:27 -0700

    starting day4

---
+++ 2022/go/day4/go.mod
@@ -0,0 +1,3 @@
+module aoc22d4
+
+go 1.20
---
+++ 2022/go/day4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	filePath := os.Args[1]
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+}
