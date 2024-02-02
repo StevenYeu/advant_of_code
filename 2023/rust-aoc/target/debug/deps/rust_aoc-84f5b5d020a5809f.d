475b0b5854075e82905d5c9cfa8563a7af653de9
Steven Yeu <syeu@sdsc.edu>
2024-02-02 10:26:50 -0800

    remove target

--- 2023/rust-aoc/target/debug/deps/rust_aoc-84f5b5d020a5809f.d
+++
@@ -1,6 +0,0 @@
-/Users/stevenyeu/Projects/aoc/2023/rust-aoc/target/debug/deps/rust_aoc-84f5b5d020a5809f.rmeta: src/main.rs src/days.rs
-
-/Users/stevenyeu/Projects/aoc/2023/rust-aoc/target/debug/deps/rust_aoc-84f5b5d020a5809f.d: src/main.rs src/days.rs
-
-src/main.rs:
-src/days.rs:
--- 2023/rust-aoc/target/debug/rust-aoc.d
+++
@@ -1,1 +0,0 @@
-/Users/stevenyeu/Projects/aoc/2023/rust-aoc/target/debug/rust-aoc: /Users/stevenyeu/Projects/aoc/2023/rust-aoc/src/days.rs /Users/stevenyeu/Projects/aoc/2023/rust-aoc/src/main.rs
