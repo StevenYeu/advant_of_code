527663d9c329cf216d3588f4357821c0e0efde59
Steven Yeu <stevenhanhyeu@gmail.com>
2024-08-21 17:43:50 -0700

    complete day 1 part 1 with zig

--- 2023/zig/src/aoc.zig
+++ 2023/zig/src/aoc.zig
@@ -1,33 +1,35 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub fn dayOne() u32 {
+pub fn dayOne() i32 {
     const allocator = std.heap.page_allocator;
     const buf = std.fs.cwd().readFileAlloc(allocator, "example_one.txt", std.math.maxInt(usize)) catch |err| {
         std.debug.print("{}\n", .{err});
         return 0;
     };
     var it = std.mem.tokenize(u8, buf, "\n");
+    var sum: i32 = 0;
     while (it.next()) |line| {
         var line_nums: [2]i32 = [_]i32{ -1, -1 };
-        var sum: i32 = 0;
+        var line_sum: i32 = 0;
         for (line) |c| {
             if ((c > 47) and (c < 58)) {
-                std.debug.print("{c}", .{c});
                 const val = (c - '0');
                 push(&line_nums, val);
             }
-            sum += line_nums[0] + line_nums[1];
+            if (line_nums[1] == -1) {
+                line_nums[1] = line_nums[0];
+            }
         }
-        std.debug.print("\n", .{});
-        std.debug.print("Line total is {d}\n", .{sum});
+        line_sum = (line_nums[0] * 10) + line_nums[1];
+        sum += line_sum;
     }
 
     allocator.free(buf);
-    return 9;
+    return sum;
 }
 
-pub fn pop(arr: *[]i32) void {
+pub fn pop(arr: *[2]i32) void {
     if (arr[1] != -1) {
         arr[1] = -1;
         return;
@@ -37,15 +39,16 @@ pub fn pop(arr: *[]i32) void {
         return;
     }
 }
-pub fn push(arr: *[]i32, val: i32) void {
-    if (arr[0] != -1) {
+pub fn push(arr: *[2]i32, val: i32) void {
+    if (arr[0] == -1) {
         arr[0] = val;
         return;
     }
-    if (arr[1] != -1) {
+    if (arr[1] == -1) {
         arr[1] = val;
         return;
     }
+    arr[1] = val;
 }
 
 test push {}
